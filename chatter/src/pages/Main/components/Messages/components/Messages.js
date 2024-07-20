defa280288e18c0af6a3206db4bcfff1d4677562
Nicolas <ngonzales@homecityice.com>
2024-07-19 21:25:50 -0300

    removed some changes

--- chatter/src/pages/Main/components/Messages/components/Messages.js
+++ chatter/src/pages/Main/components/Messages/components/Messages.js
@@ -7,16 +7,16 @@ import { AXIOS_INSTANCE, socket } from "../../../../../config";
 import { ENDPOINTS, SOCKET_EVENTS } from "../../../../../constants";
 import Message from "./Message";
 
+const INITIAL_TYPING_STATE = {
+  typingNow: false,
+  typingFrom: undefined,
+};
+
 function Messages({ activeContact, currentUserId }) {
-  const [isTyping, setIsTyping] = useState({
-    typingNow: false,
-    typingFrom: undefined,
-  });
+  const [isTyping, setIsTyping] = useState(INITIAL_TYPING_STATE);
 
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [previousContactId, setPreviousContactId] = useState(null);
-  const prevMessageRef = useRef(message);
 
   const handleSendMessage = useCallback(() => {
     const temporaryMessage = {
@@ -65,33 +65,17 @@ function Messages({ activeContact, currentUserId }) {
   }, []);
 
   useEffect(() => {
-    if (activeContact?.id !== previousContactId) {
-      if (previousContactId) {
-        socket.emit(SOCKET_EVENTS.USER_EVENTS.USER_STOP_TYPING, {
-          senderId: currentUserId,
-          receiverId: previousContactId,
-        });
-      }
-      setPreviousContactId(activeContact?.id);
-    }
-  }, [activeContact, previousContactId, currentUserId]);
-
-  useEffect(() => {
-    const prevMessage = prevMessageRef.current;
-
     if (message) {
       socket.emit(SOCKET_EVENTS.USER_EVENTS.USER_TYPING, {
         senderId: currentUserId,
         receiverId: activeContact?.id,
       });
-    } else if (prevMessage && !message) {
+    } else {
       socket.emit(SOCKET_EVENTS.USER_EVENTS.USER_STOP_TYPING, {
         senderId: currentUserId,
         receiverId: activeContact?.id,
       });
     }
-
-    prevMessageRef.current = message;
   }, [message, currentUserId, activeContact]);
 
   useEffect(() => {
